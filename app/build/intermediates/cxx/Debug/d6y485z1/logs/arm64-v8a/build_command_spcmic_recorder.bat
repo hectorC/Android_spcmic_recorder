fcb97303f549558d91c870e92747118fe698bc31
Hector Centeno <hcengar@gmail.com>
2025-10-03 16:49:07 -0400

    Update .gitignore and remove unnecessary files from tracking

--- app/build/intermediates/cxx/Debug/d6y485z1/logs/arm64-v8a/build_command_spcmic_recorder.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\hcenteno\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\Audio_Projects\\Android_spcmic_recorder\\app\\.cxx\\Debug\\d6y485z1\\arm64-v8a" ^
-  spcmic_recorder
--- app/build/intermediates/cxx/Debug/d6y485z1/logs/x86_64/build_command_spcmic_recorder.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\hcenteno\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\Audio_Projects\\Android_spcmic_recorder\\app\\.cxx\\Debug\\d6y485z1\\x86_64" ^
-  spcmic_recorder
